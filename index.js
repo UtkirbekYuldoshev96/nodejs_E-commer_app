2c8a513100c0c9586a1fa65f5a8cc28ac1108640
Utkirbek <utkirbekyuldoshev7@gmail.com>
2024-01-22 17:44:56 +0500

    e-commers templates

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const http = require('http');
 const fs = require('fs');
 
+// SERVER
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/Data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
       const pathName = req.url;
-
+      
+      // Overview page
       if (pathName === '/' || pathName === '/overview') {
-            res.end('This is the overview');
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end(tempOverview);
+      
+      // Product page
       } else if (pathName === '/product') {
-            res.end('This is the Products');
-      } else if (pathName === '/api') {
-            fs.readFile(`${__dirname}/Data/data.json`, 'utf-8', (err, data) => {
-                  const productData = JSON.parse(data);
-                  res.writeHead(200, {'Content-type': 'application/json'});
-                  res.end(data);
-                  // console.log(productData);
-            });
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end(tempProduct);
 
-            
-            // res.end('This is the Api');
+      // API page
+      } else if (pathName === '/api') {
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(data);
+
+      // Not found
       } else {
             res.writeHead(404, {
                   'Content-type': 'text/html',
@@ -26,7 +35,6 @@ const server = http.createServer((req, res) => {
             res.end('<h1>Page not found</h1>');
       }
 });
-
 
 server.listen(3000, '127.0.0.1', () => {
       console.log('Server is listening on port 3000');
